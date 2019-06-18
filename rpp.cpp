55f4cee176cba7dc1b545bbf92847db2a60b3d50
Erik Valkering <erikvalkering@gmail.com>
2019-06-18 20:00:17 +0200

    Added example for derived state

--- rpp.cpp
+++ rpp.cpp
@@ -10,40 +10,37 @@ int main() {
     auto x = observable{0};
     auto y = observable{2};
     
-    // auto z = computed{[&](auto &ctx){
-    //     if (b(ctx))
-    //         return x(ctx) + 1;
-    //     else
-    //         return y(ctx) + 1;
-    // }};
+     auto z = computed{[&] {
+         if (b())
+             return x();
+         else
+             return y();
+     }};
     
     // "x = 0"
     auto_run([&] {
-        if (b())
-            cout << "x = " << x() << endl;
-        else
-            cout << "y = " << y() << endl;
+        cout << "z = " << z() << endl;
     });
 
     cout << "x = 10: --> ";
-    x = 10; // "x = 10"
+    x = 10; // "z = 10"
     cout << endl;
     cout << "y = 12: --> ";
     y = 12;
     cout << endl;
 
     cout << "b = false: --> ";
-    b = false; // "y = 12"
+    b = false; // "z = 12"
     cout << endl;
 
     cout << "x = 100: --> ";
     x = 100;
     cout << endl;
     cout << "y = 102: --> ";
-    y = 102; // "y = 102"
+    y = 102; // "z = 102"
     cout << endl;
 
     cout << "b = true: --> ";
-    b = true; // "x = 100"
+    b = true; // "z = 100"
     cout << endl;
 }
