3a3656315908e0e577fc86f93603ca412f04f828
Erik Valkering <erikvalkering@gmail.com>
2019-06-22 17:48:43 +0200

    Updated example

--- rpp.cpp
+++ rpp.cpp
@@ -10,37 +10,41 @@ int main() {
     auto x = observable{0};
     auto y = observable{2};
     
-     auto z = computed{[&] {
-         if (b())
-             return x();
-         else
-             return y();
-     }};
-    
-    // "x = 0"
-    auto_run([&] {
-        cout << "z = " << z() << endl;
-    });
+    auto z = computed{[&] {
+        cout << "[computing z...] ";
 
-    cout << "x = 10: --> ";
+        if (b())
+            return x();
+        else
+            return y();
+    }};
+
+    cout << "auto_run 1";
+    auto_run([&] { cout << "z = " << z(); });
+    cout << endl;
+    cout << "auto_run 2";
+    auto_run([&] { cout << "z + 1 = " << z() << " "; });
+    cout << endl;
+
+    cout << "x = 10: -->";
     x = 10; // "z = 10"
     cout << endl;
-    cout << "y = 12: --> ";
+    cout << "y = 12: -->";
     y = 12;
     cout << endl;
 
-    cout << "b = false: --> ";
+    cout << "b = false: -->";
     b = false; // "z = 12"
     cout << endl;
 
-    cout << "x = 100: --> ";
+    cout << "x = 100: -->";
     x = 100;
     cout << endl;
-    cout << "y = 102: --> ";
+    cout << "y = 102: -->";
     y = 102; // "z = 102"
     cout << endl;
 
-    cout << "b = true: --> ";
+    cout << "b = true: -->";
     b = true; // "z = 100"
     cout << endl;
 }
